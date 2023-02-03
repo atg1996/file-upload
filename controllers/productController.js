3e90ac79c09136f074a507c867ed67fd8b17b977
Tigran Asatryan <t.asatryan96@gmail.com>
2023-02-03 15:21:28 +0400

    minor checks

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,8 +4,9 @@ const {StatusCodes} = require("http-status-codes")
 
 const createProduct = async(req,res) => {
 
-    res.send("create product api");
-
+    console.log(req.body);
+    const product = await Product.create(req.body)
+    res.status(StatusCodes.CREATED).json({product})
 }
 
 const getAllProducts = async(req,res) => {
